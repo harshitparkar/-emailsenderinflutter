8db0f0abb08a51d8d5210b18f05bf567876e9c28
harshitparkar <harshit.parkar@sakec.ac.in>
2020-09-16 18:29:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:emailsender/utils.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Email"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Utils.openEmail(toEmail:'HIS_OWN_EMAILID@gmail.com, SUPERVISOR@gmail.com',
+                subject: 'Project Name',
+                body:"Your File HERE");
+              },
+              child: Text("Email This"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
